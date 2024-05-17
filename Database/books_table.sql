0e696cbcbf37915eec78b93d61de77e72bd352b0
VandevilleF <flo.vandeville@gmail.com>
2024-05-17 15:35:41 +0200

    Change name of colum name for username and add colum release date in books table

--- Database/books_table.sql
+++ Database/books_table.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS Books(
    title VARCHAR(256) NOT NULL,
    author VARCHAR(50) NOT NULL,
    editor VARCHAR(50) NOT NULL,
+   release_date VARCHAR(50) NOT NULL,
    couverture VARCHAR(256)
 );
