abc02cc1ac54486f506b850909b9e5ba2b41878a
VandevilleF <flo.vandeville@gmail.com>
2024-05-17 16:45:37 +0200

    Change type of ISBN and book_id to varchar

--- Database/user_books_table.sql
+++ Database/user_books_table.sql
@@ -1,7 +1,7 @@
 USE MyLib;
 CREATE TABLE User_books(
    user_ID INT,
-   book_ID INT,
+   book_ID VARCHAR(25),
    status_ID INT,
    FOREIGN KEY(user_ID) REFERENCES Users(ID),
    FOREIGN KEY(book_ID) REFERENCES Books(ISBN),
