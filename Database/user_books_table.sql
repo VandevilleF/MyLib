342b36eeb0c292fc6b1aae0ec6311119098b3397
VandevilleF <flo.vandeville@gmail.com>
2024-06-03 15:54:59 +0200

    Remove sql files for creation of table and add all in the same file

--- Database/user_books_table.sql
+++
@@ -1,9 +0,0 @@
-USE MyLib;
-CREATE TABLE User_books(
-   user_ID INT,
-   book_ID VARCHAR(25),
-   status_ID INT,
-   FOREIGN KEY(user_ID) REFERENCES Users(ID),
-   FOREIGN KEY(book_ID) REFERENCES Books(ISBN),
-   FOREIGN KEY(status_ID) REFERENCES Status(ID)
-);
