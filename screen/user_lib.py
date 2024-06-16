445188abc334d42ff41375dd5f86ae00f36045e4
VandevilleF <flo.vandeville@gmail.com>
2024-06-17 00:05:23 +0200

    Add condition if no book in library for result white square on screen, change display with 2 label and add "détailsé button for display info of book

--- screen/user_lib.py
+++ screen/user_lib.py
@@ -3,7 +3,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import AsyncImage
 from kivy.uix.label import Label
-from kivy.app import App
+from kivy.uix.button import Button
 from utils import popup_error, conn_to_ddb, get_user_id_jwt
 
 
@@ -34,17 +34,30 @@ class UserLib(Screen):
         container = self.ids.container
         container.clear_widgets()
 
+        if all(row == (None, None, None) for row in result):
+            self.ids.n_element.text = "Pas de livre"
+            return
         # Display the books in the user's library
         for book in result:
-            box_lay = BoxLayout(orientation='vertical', size_hint_y=None, spacing=10)
+            box_lay = BoxLayout(orientation='vertical', size_hint_y=None, spacing=5)
             image = AsyncImage(source=book[0], size_hint=(1, None), height=50)
-            lab_info = Label(text=f"{book[1]}\n{book[2]}",
-                             text_size=(container.width, None), font_size=14,
-                             halign='center', valign='middle')
-
+            lab_title = Label(text=f"{book[1]}",
+                              text_size=(container.width, None), font_size=14,
+                              halign='center', valign='middle', shorten=True,
+                              shorten_from='right')
+            lab_author = Label(text=f"{book[2]}",
+                               text_size=(container.width, None), font_size=14,
+                               halign='center', valign='middle', shorten=True,
+                               shorten_from='right')
+            detail = Button(text="Détails", font_size=10, size_hint=(0.5, 1),
+                            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+                            background_color=(0, 0, 0, 0))
             # Add the book information to the boxlayout
             box_lay.add_widget(image)
-            box_lay.add_widget(lab_info)
+            box_lay.add_widget(lab_title)
+            box_lay.add_widget(lab_author)
+            box_lay.add_widget(detail)
+            detail.bind(on_press=self.button_click)
 
             # Add the boxlayout containing the book information to the container
             container.add_widget(box_lay)
@@ -55,3 +68,6 @@ class UserLib(Screen):
         # Update the label showing the number of books
         self.ids.n_element.text = f"Nombre de livres {len(result)}"
 
+    def button_click(self, instance):
+        """Open the book screen when the button is clicked"""
+        self.manager.current = 'Book'
