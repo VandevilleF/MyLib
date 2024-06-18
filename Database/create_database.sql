cda0b6a385f2b04eab4110fb493400fba179586c
VandevilleF <flo.vandeville@gmail.com>
2024-06-18 20:29:00 +0200

    Add new column for save note on the current user's book

--- Database/create_database.sql
+++ Database/create_database.sql
@@ -24,6 +24,7 @@ CREATE TABLE User_books(
    user_ID INT,
    book_ID VARCHAR(25),
    status_ID INT,
+   note VARCHAR(256),
    FOREIGN KEY(user_ID) REFERENCES Users(ID),
    FOREIGN KEY(book_ID) REFERENCES Books(ISBN),
    FOREIGN KEY(status_ID) REFERENCES Status(ID)
