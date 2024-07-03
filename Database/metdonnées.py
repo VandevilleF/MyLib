9ff9c063c49e4ddb0b1c73d3ce6c5a02b9276f16
VandevilleF <flo.vandeville@gmail.com>
2024-07-03 18:45:11 +0200

    Add summary in scrap and summary column

--- Database/metdonnées.py
+++ Database/metdonnées.py
@@ -2,24 +2,20 @@
 
 import json
 from mysql import connector
+from utils import conn_to_ddb
 
 # Charger le fichier JSON
 with open('data.json', 'r') as file:
     data = json.load(file)
 
 # Connexion à la base de données MySQL
-conn = connector.connect(
-    host='localhost',
-    user='user02',
-    password='user02pwd',
-    database='MyLib'
-)
+conn = conn_to_ddb()
 cursor = conn.cursor()
 
 # Préparer la requête d'insertion
 insert_query = """
-INSERT INTO Books (ISBN, title, author, editor, release_date, couverture)
-VALUES (%s, %s, %s, %s, %s, %s)
+INSERT INTO Books (ISBN, title, author, editor, release_date, couverture, summary)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
 """
 
 # Insérer les données
@@ -27,7 +23,8 @@ for entry in data:
     try:
         cursor.execute(insert_query,
                        (entry['ISBN'], entry['Titre'], entry['Auteur'],
-                        entry['Editeur'], entry['Date de sortie'], entry['Couverture']))
+                        entry['Editeur'], entry['Date de sortie'],
+                        entry['Couverture'], entry['Résumé']))
     except connector.IntegrityError as e:
         print(f"Ignorer l'insertion pour ISBN {entry['ISBN']}. Doublon détecté.")
 
